92be8683dae0e0530e4a76a2d34092a03bcfe359
Faiz <tenkois@gmail.com>
2018-11-30 18:10:15 +0800

    solved

--- init.rb
+++ init.rb
@@ -1,13 +1,52 @@
 require 'sinatra'
 
 get '/' do
-
   @page_title = "Welcome to Exploring California"
+  @body_id = "home"
   erb :index
 end
 
-get '/test' do
-  template = " The current year is <%= Time.now.year %>"
-  erb template
+get '/contact' do
+  @page_title = "Explorer California: Contact Us"
+  erb :contact
+end
 
+get '/explorers' do
+  @page_title = "Explorer California: Explorers"
+  erb :explorers
+end
+
+get '/mission' do
+  @page_title = "Explorer California: Mission"
+  erb :mission
+end
+
+get '/resources' do
+  @page_title = "Explorer California: Resources"
+  erb :resources
+end
+
+get '/resources/faq' do
+  @page_title = "Explorer California: FAQ"
+  erb :'resources/faq'.to_sym
+end
+
+get '/support' do
+  @page_title = "Explorer California: Support"
+  erb :support
+end
+
+get '/tours' do
+  @page_title = "Explorer California: Tours"
+  erb :tours
+end
+
+get '/tours/tour_detail_backpack' do
+  @page_title = "Explorer California: Tours- Backpack cal"
+  erb :'tours/tour_details_backpack'.to_sym
+end
+
+get '/tours/tour_detail_bigsur' do
+  @page_title = "Explorer California: Tours -  Bigsur Retreat"
+  erb :'tours/tour_details_bigsur'.to_sym
 end
